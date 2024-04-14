3e87c5f7540d9f636388757bcef4091bd30375c3
Mohamed Amjar <amjar.mohamed.rh@gmail.com>
2024-04-14 08:26:48 +0100

    new updates !

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import "./App.css";
-import { FaPlus } from "react-icons/fa6";
-import { IoMdSearch } from "react-icons/io";
-import { FaRegSquare } from "react-icons/fa";
-import { FaCheckSquare } from "react-icons/fa";
-import { FaDeleteLeft } from "react-icons/fa6";
-import { useState, useRef } from "react";
+import './App.css';
+import { FaPlus } from 'react-icons/fa6';
+import { IoMdSearch } from 'react-icons/io';
+import { FaRegSquare } from 'react-icons/fa';
+import { FaCheckSquare } from 'react-icons/fa';
+import { FaDeleteLeft } from 'react-icons/fa6';
+import { useState, useRef } from 'react';
 
-import Completed from "./components/Completed";
-import Active from "./components/Active";
-import TodosSearch from "./components/Search";
+import Completed from './components/Completed';
+import Active from './components/Active';
+import TodosSearch from './components/Search';
 /* 
 
 
@@ -22,7 +22,7 @@ function App() {
 
   // add new todo if input is not empty, if empty set focus to the input
   const addNewTodo = () => {
-    if (inputRef.current.value.trim() === "") {
+    if (inputRef.current.value.trim() === '') {
       inputRef.current.focus();
     } else {
       setTodos([
@@ -33,13 +33,13 @@ function App() {
           time: new Date().toISOString(),
         },
       ]);
-      inputRef.current.value = "";
+      inputRef.current.value = '';
     }
   };
 
   // if the user clicked enter key in the input el , should add new todo if not empty
   const enterClick = (event) => {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       addNewTodo();
     }
   };
@@ -61,13 +61,13 @@ function App() {
 
   const filterdTodos = (filter) => {
     switch (filter) {
-      case "completed":
+      case 'completed':
         setFiltedCategories(Completed(todos));
         break;
-      case "active":
+      case 'active':
         setFiltedCategories(Active(todos));
         break;
-      case "all":
+      case 'all':
         setFiltedCategories([...todos]);
         break;
       default:
@@ -87,24 +87,24 @@ function App() {
   };
 
   return (
-    <div className="App w-3/4 min-h-fit mx-auto border border-neutral-800 bg-neutral-200 bg-opacity-50 text-neutral-900 shadow-lg ">
-      <h1 className="text-center py-5 mb-2 font-bold leading-tight text-neutral-800 ">
+    <div className='App w-3/4 min-h-fit mx-auto border border-neutral-800 bg-neutral-200 bg-opacity-50 text-neutral-900 shadow-lg '>
+      <h1 className='text-center py-5 mb-2 font-bold leading-tight text-neutral-800 '>
         THINGS TO DO .
       </h1>
-      <div className="p-5">
+      <div className='p-5'>
         <input
-          type="text"
-          className="w-full p-2 block border rounded mb-5 focus:border-none focus:outline-none"
-          placeholder="Add New Task"
+          type='text'
+          className='w-full p-2 block border rounded mb-5 focus:border-none focus:outline-none'
+          placeholder='Add New Task'
           ref={inputRef}
           onKeyDown={(e) => enterClick(e)}
         />
 
-        <TodosSearch />
+        {/* <TodosSearch /> */}
 
-        <ul className="pb-10 text-neutral-500">
+        <ul className='pb-10 text-neutral-500'>
           {todos.length === 0 && (
-            <li className=" p-2 border-b-2 text-center  ">
+            <li className=' p-2 border-b-2 text-center  '>
               Nothing to do, Add New...
             </li>
           )}
@@ -113,18 +113,18 @@ function App() {
               return (
                 <>
                   <li
-                    className="flex  items-center justify-between content-center p-2 border-b border-neutral-900 check-uncheck-todo hover:bg-slate-100 hover:bg-opacity-50 transition "
+                    className='flex  items-center justify-between content-center p-2 border-b border-neutral-900 check-uncheck-todo hover:bg-slate-100 hover:bg-opacity-50 transition '
                     key={index}
                   >
                     {item.completed ? (
                       <FaCheckSquare
-                        className="cursor-pointer  "
+                        className='cursor-pointer  '
                         size={20}
                         onClick={() => doneTodo(index)}
                       />
                     ) : (
                       <FaRegSquare
-                        className="cursor-pointer  "
+                        className='cursor-pointer  '
                         size={20}
                         onClick={() => doneTodo(index)}
                       />
@@ -132,19 +132,19 @@ function App() {
 
                     <p
                       className={`text-left flex-grow  px-5 break-words  ${
-                        item.completed ? "line-through" : ""
+                        item.completed ? 'line-through' : ''
                       }`}
                     >
                       {/* line-through */}
                       {item.todo}
                     </p>
-                    <time dateTime={item.time} className="break-words">
+                    <time dateTime={item.time} className='break-words'>
                       {/* {currentTime(item.time)} */}
                       {item.time}
                     </time>
-                    <p className="pl-4">
+                    <p className='pl-4'>
                       <FaDeleteLeft
-                        className=" cursor-pointer remove-todo"
+                        className=' cursor-pointer remove-todo'
                         size={20}
                         onClick={() => removeTodo(index)}
                       />
@@ -156,48 +156,48 @@ function App() {
           })}
 
           {todos.length > 5 && (
-            <li className=" p-2 border-b-2  text-center  cursor-pointer select-none">
+            <li className=' p-2 border-b-2  text-center  cursor-pointer select-none'>
               Load More Todos...
             </li>
           )}
         </ul>
       </div>
-      <div className="flex  bg-neutral-900 border-t-2 mt-4 py-2  px-2 flex-auto items-center content-evenly justify-between cursor-pointer select-none">
-        <div className="serach  flex justify-evenly  items-center content-between flex-auto">
+      <div className='flex  bg-neutral-900 border-t-2 mt-4 py-2  px-2 flex-auto items-center content-evenly justify-between cursor-pointer select-none'>
+        <div className='serach  flex justify-evenly  items-center content-between flex-auto'>
           <p>
             <FaPlus
               size={20}
               onClick={() => addNewTodo()}
-              className="text-neutral-200 hover:text-cyan-500 transition duration-300 ease-linear transform hover:scale-105"
+              className='text-neutral-200 hover:text-cyan-500 transition duration-300 ease-linear transform hover:scale-105'
             />
           </p>
           <p>
             <IoMdSearch
               size={20}
-              className=" text-neutral-400 hover:text-cyan-500 transition duration-300 ease-linear transform hover:scale-105"
+              className=' text-neutral-400 hover:text-cyan-500 transition duration-300 ease-linear transform hover:scale-105'
             />
           </p>
 
-          <p className="border-l-2  pl-5 text-neutral-400">
+          <p className='border-l-2  pl-5 text-neutral-400'>
             {todos.length} items left
           </p>
         </div>
-        <div className="filter flex justify-evenly items-center content-between  flex-auto text-neutral-400">
+        <div className='filter flex justify-evenly items-center content-between  flex-auto text-neutral-400'>
           <p
-            className=" hover:text-cyan-500 transition duration-300 ease-linear transform hover:scale-105"
-            onClick={() => filterdTodos("all")}
+            className=' hover:text-cyan-500 transition duration-300 ease-linear transform hover:scale-105'
+            onClick={() => filterdTodos('all')}
           >
             All
           </p>
           <p
-            className=" hover:text-cyan-500 transition duration-300 ease-linear transform hover:scale-105"
-            onClick={() => filterdTodos("active")}
+            className=' hover:text-cyan-500 transition duration-300 ease-linear transform hover:scale-105'
+            onClick={() => filterdTodos('active')}
           >
             Active
           </p>
           <p
-            className=" hover:text-cyan-500 transition duration-300 ease-linear transform hover:scale-105 visited:text-cyan-500"
-            onClick={() => filterdTodos("completed")}
+            className=' hover:text-cyan-500 transition duration-300 ease-linear transform hover:scale-105 visited:text-cyan-500'
+            onClick={() => filterdTodos('completed')}
           >
             Completed
           </p>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,16 @@
-import { useRef, useState } from "react";
-import Completed from "./Completed";
+import { useRef, useState } from 'react';
+import Completed from './Completed';
 const TodosSearch = (mydata = []) => {
   const [data, usedata] = useState([]);
-  const [search, useSearch] = useState("");
+  const [search, useSearch] = useState('');
   const inputRef = useRef([]);
   // search clicked hide input search and replaced with search input .
   usedata([...mydata]);
 
   const enterClick = (event) => {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       todosfiletr(inputRef.current.values, data);
-      inputRef.current.value = "";
+      inputRef.current.value = '';
     }
   };
 
@@ -25,9 +25,9 @@ const TodosSearch = (mydata = []) => {
 
   return (
     <input
-      type="saerch"
-      className="w-full p-2 block border rounded mb-5 focus:border-none focus:outline-none"
-      placeholder="Search for a Todo..."
+      type='saerch'
+      className='w-full p-2 block border rounded mb-5 focus:border-none focus:outline-none'
+      placeholder='Search for a Todo...'
       ref={inputRef}
       onKeyDown={(e) => enterClick(e)}
     />
